0b4125834027280ed05503855cc34083f84a6cb9
Ryan Yip <ryannyipis@gmail.com>
2017-03-23 18:48:39 -0700

    implemented scoring based on speed

--- src/com/ryandev/GameState.java
+++ src/com/ryandev/GameState.java
@@ -8,20 +8,21 @@ public class GameState {
     public static void main(String[] args) {
         NatoGame game = new NatoGame();
         Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the NATO Phonetic Alphabet Trainer.\n");
         boolean playAgain = true;
         while (playAgain) {
-            game.play(input);
+            game.play();
             System.out.println("Do you want to play again? ([Y]/n)");
             String playInput = input.nextLine();
             if (playInput.equalsIgnoreCase("n") || playInput.equalsIgnoreCase("no")) {
                 playAgain = false;
             }
         }
-        input.close();
         List<Score> allScores = game.endGame();
         for (int i = 0; i < allScores.size(); i++) {
             System.out.println(String.format("Round %d Score %d/%d", i+1, allScores.get(i).getPoints(), allScores.get(i).getMaxPoints()));
         }
+        input.close();
     }
 
 }
--- src/com/ryandev/NatoGame.java
+++ src/com/ryandev/NatoGame.java
@@ -8,46 +8,49 @@ import java.util.Scanner;
  */
 public class NatoGame {
 
+    private static final int MAX_POINTS = 3000;
+
     private Scoreboard scoreboard;
 
     public NatoGame() {
         this.scoreboard = new Scoreboard();
     }
 
-    public void play(Scanner input) {
+    public void play() {
+        Scanner input = new Scanner(System.in);
+        int rounds = 5;
         int points = 0;
-        System.out.println("Welcome to the NATO Phonetic Alphabet Trainer.");
-        System.out.println("How many rounds do you want to play?");
-        int rounds = 0;
-        while (rounds <= 0) {
-            try {
-                rounds = Integer.parseInt(input.nextLine());
-                if (rounds <= 0) {
-                    System.out.println("Please input a number greater than 0.");
-                }
-            } catch (NumberFormatException nfe) {
-                System.out.println("Please input a number greater than 0.");
-            }
-        }
-
         System.out.println("Guess the NATO Phonetic word for the letter shown.");
         NatoAlphabet natoWord = new NatoAlphabet();
         for (int i = 0; i < rounds; i++) {
             natoWord.getNewNatoWord();
             System.out.println("The letter is: " + natoWord.getCurrentLetter());
+            long timeStart = System.currentTimeMillis();
             String guess = input.nextLine();
+            long timeStop = System.currentTimeMillis();
             boolean result = natoWord.checkGuess(guess);
             if (result) {
-                points++;
-                System.out.println("Your guess was CORRECT!");
+                // TODO: figure out how to make more accurate time based scoring
+                // points added for buffer time before and after input.nextLine()
+                // Unsure of how to measure time accurately for an input
+                long pointsForRound = MAX_POINTS - (timeStop - timeStart) + 2500;
+                if (pointsForRound > 0) {
+                    if (pointsForRound > MAX_POINTS) {
+                        pointsForRound = MAX_POINTS;
+                    }
+                    points += pointsForRound;
+                } else {
+                    pointsForRound = 0;
+                }
+                System.out.println("Your guess was CORRECT! You got " + pointsForRound + " points.\n");
             } else {
                 System.out.println("Your guess was INCORRECT!");
-                System.out.println("The correct answer was " + natoWord.getCurrentNatoWord());
+                System.out.println("The correct answer was " + natoWord.getCurrentNatoWord() + "\n");
             }
         }
 
         System.out.println("Your score for the round was " + points);
-        scoreboard.addScore(points, rounds);
+        scoreboard.addScore(points, rounds * MAX_POINTS);
     }
 
     public List<Score> endGame() {
